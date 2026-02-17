278f18a558c39a02a6970a1f3f4bf2756606be14
Madeline Garner <madelinegarner583@u.boisestate.edu>
2026-02-17 10:07:39 -0700

    Updated top level

--- lab-05/top.v
+++ lab-05/top.v
@@ -1,1 +1,24 @@
-// Implement top level module
+module top (
+    input  [6:0]sw,
+    output [1:0]led
+);
+
+    wire output_A;
+    assign led[0] = output_A;
+
+    circuit_a a_inst (
+        .A(sw[0]),
+        .B(sw[1]),
+        .C(sw[2]),
+        .D(sw[3]),
+        .Y(output_A)
+    );
+
+    circuit_b b_inst (
+        .A(output_A),
+        .B(sw[4]),
+        .C(sw[5]),
+        .D(sw[6]),
+        .Y(led[1])
+    );
+endmodule
