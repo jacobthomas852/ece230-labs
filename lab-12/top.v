f524c49b5abe7ff4cc44827ee8b55fc13473b6a2
Edgar <edgariosnegrete@icloud.com>
2026-04-14 10:50:03 -0600

    Instantiate one-hot and binary state machines

--- lab-12/top.v
+++ lab-12/top.v
@@ -6,5 +6,25 @@ module top(
 );
 
     // Hook up binary and one-hot state machines
+    one_hot oh(
+        .w(sw),
+        .clk(btnC),
+        .reset(btnu),
+        .z(led[0]),
+        .Astate(led[2]),
+        .Bstate(led[3]),
+        .Cstate(led[4]),
+        .Dstate(led[5]),
+        .Estate(led[6])
+    );
+    
+    binary bin(
+        .w(sw),
+        .clk(btnC),
+        .reset(btnU), 
+        .z(led[1]),
+        .State(led[9:7])
+
+    );
 
 endmodule
